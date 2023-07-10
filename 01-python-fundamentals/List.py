686f734e62c3c0507b995a87858c62c2eeed3402
unknown <yad.juini@gmail.com>
2023-07-10 22:22:36 +0200

    #

--- 01-python-fundamentals/List.py
+++ 01-python-fundamentals/List.py
@@ -12,24 +12,25 @@ sports_directory = {
 z = [ {'x': 10, 'y': 20} ]
 
 
+x[1][0]= 15 
 
-# x[1][0]= 15 aaa
-
-# print(x)
-
+print(x)
 # ...........10 -> 15 ..........
 
-# students[0]['last_name'] = 'Bryant'
-# print(students)
+
+students[0]['last_name'] = 'Bryant'
+print(students)
 # ........Jordan->Bryant.......
 
-# sports_directory['soccer'][0] ='Andres'
-# print(sports_directory['soccer'])
+
+sports_directory['soccer'][0] ='Andres'
+print(sports_directory['soccer'])
 # ..........Messi -> Andres...........
 
-# z[0]['y'] = 30
-# print(z)
 
+z[0]['y'] = 30
+print(z)
+# ..........20 ->30..............
 
                 # 2. Iterate Through a List of Dictionaries
 
@@ -39,22 +40,22 @@ students = [
 {'first_name' : 'Mark', 'last_name' : 'Guillen'},
 {'first_name' : 'KB', 'last_name' : 'Tonel'}
 ]
-# def iterateDictionary(some_list):
-#     for Dictionry in some_list:
-#         output= ', '.join([f'{key} {value}' for key, value in Dictionry.items()])
-#         print(output)
-# iterateDictionary(students) 
+def iterateDictionary(some_list):
+    for Dictionry in some_list:
+        output= ', '.join([f'{key} {value}' for key, value in Dictionry.items()])
+        print(output)
+iterateDictionary(students) 
 
 
                 # 3. Get Values From a List of Dictionaries
 
-# def iterateDictionary2(key_name, some_list):
-#     for dictionary in some_list:
-#         if key_name in dictionary:
-#             value = dictionary[key_name]
-#             print(value)
-# iterateDictionary2("first_name", students)
-# iterateDictionary2('last_name', students)
+def iterateDictionary2(key_name, some_list):
+    for dictionary in some_list:
+        if key_name in dictionary:
+            value = dictionary[key_name]
+            print(value)
+iterateDictionary2("first_name", students)
+iterateDictionary2('last_name', students)
 
 
                 # 4. Iterate Through a Dictionary with List Values
---
+++ 01-python-fundamentals/for_loop_basicl.py
@@ -0,0 +1,41 @@
+# ---------Basic---------
+for i in range(0,151):
+    print(i)
+
+# -----------Multiples of five -------
+for i in range(5,1001):
+    if i%5==0:
+        print(i)
+
+# ---------Counting the Dojo Way--------
+
+for i in range(1,101):
+    if i%5==0:
+        print(i,"coding")
+    if i%10==0:
+        print(i,"coding dojo")
+    else: print(i)
+
+# ----------Sucker's Huge-------
+
+sum=0
+for i in range(0,500001):
+    if i%2!=0:
+        sum+=i
+print(sum)
+
+# ----------countdown By Fours--------
+
+for i in range(2018,-1,-4):
+    print(i)
+
+
+# ------Flexible Counter------------
+
+def Flex(lowNum,highNum,multi):
+    for i in range(lowNum,highNum+1):
+        if i%multi==0:
+            print(i)
+    return None
+
+Flex(2,9,3)
