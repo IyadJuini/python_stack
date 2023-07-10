e551cb89251cd68b304004414eb7281501595968
unknown <yad.juini@gmail.com>
2023-07-10 23:36:00 +0200

    assignement_python

--- 01-python-fundamentals/for_loop_basicl.py
+++ 01-python-fundamentals/for_loop_basicl.py
@@ -1,41 +1,42 @@
 # ---------Basic---------
-for i in range(0,151):
-    print(i)
+# for i in range(0,151):
+#     print(i)
 
 # -----------Multiples of five -------
-for i in range(5,1001):
-    if i%5==0:
+# for i in range(5,1001):
+#     if i%5==0:
+#         print(i)
+
+# # ---------Counting the Dojo Way--------
+
+for i in range (1,101):
+    if i % 10 == 0  :
+        print("Coding Dojo")
+    elif i % 5 == 0 :
+        print("Coding")
+    else:
         print(i)
 
-# ---------Counting the Dojo Way--------
+# # ----------Sucker's Huge----------
 
-for i in range(1,101):
-    if i%5==0:
-        print(i,"coding")
-    if i%10==0:
-        print(i,"coding dojo")
-    else: print(i)
+# sum=0
+# for i in range(0,500001):
+#     if i%2!=0:
+#         sum+=i
+# print(sum)
 
-# ----------Sucker's Huge----------
+# # ----------countdown By Fours----------
 
-sum=0
-for i in range(0,500001):
-    if i%2!=0:
-        sum+=i
-print(sum)
-
-# ----------countdown By Fours----------
-
-for i in range(2018,-1,-4):
-    print(i)
+# for i in range(2018,-1,-4):
+#     print(i)
 
 
-# ------Flexible Counter--------------
+# # ------Flexible Counter--------------
 
-def Flex(lowNum,highNum,multi):
-    for i in range(lowNum,highNum+1):
-        if i%multi==0:
-            print(i)
-    return None
+# def Flex(lowNum,highNum,multi):
+#     for i in range(lowNum,highNum+1):
+#         if i%multi==0:
+#             print(i)
+#     return None
 
-Flex(2,9,3)
+# Flex(2,9,3)
