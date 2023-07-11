b87a6e6abddb9721d9e3fd1cdebf8d9169f0d7af
Iyadh <yad.juini@gmail.com>
2023-07-11 21:47:54 +0200

    UserBankAccount

---
+++ 02-python-oop/Core/User_BankAcount.py
@@ -0,0 +1,49 @@
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    all_accounts = []
+    def __init__(self, int_rate = 0.01, balance = 0): 
+        self.int_rate = int_rate
+        self.balance = balance
+    
+
+    def deposit(self, amount):
+        # your code here
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        # your code here
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"YOUR INT RATE :{self.int_rate} & YOUR BALANCE IS : {self.balance}")
+        # your code here
+    def yield_interest(self):
+        # your code here
+        if self.balance > 0 :
+            self.balance += self.balance * self.int_rate
+            return self
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate = 0.02, balance=0)
+    
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
+        return self
+    
+    def make_withdraw(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_user_account(self):
+        self.account.display_account_info()
+        return None
+
+
+GON = User ("Gon","yad.juini@gmail.com")
+Account1 = BankAccount(0.01, 4000)
+
+
+print (GON.account.balance)
